6712d0f4f35a19a2c0f3fc597f441eb39585665a
ATITYA RAM S <atityaramsureshmanickam@gmail.com>
2026-09-12 23:12:59 +0530

    feat: add Astro configuration

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+export default defineConfig({
+  integrations: [react()],
+  server: { port: 4321, host: true }
+});
